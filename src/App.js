a8e9312b796cb88bf3dbee098380585c532da491
dwivedimansi <mansidwivedi785@gmail.com>
2025-01-07 20:34:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f0f8ff' }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
